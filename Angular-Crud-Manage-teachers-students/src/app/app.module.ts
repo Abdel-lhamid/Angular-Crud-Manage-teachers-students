962af81772b1c00bbd49d68ad9bcc0ba7b441862
Abdel-lhamid <abdelhamidbusiness@gmail.com>
2023-03-21 15:07:41 +0000

    create component. closes #4

--- Angular-Crud-Manage-teachers-students/src/app/app.module.ts
+++ Angular-Crud-Manage-teachers-students/src/app/app.module.ts
@@ -4,10 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TeacherCreateComponent } from './teachers/teacher-create/teacher-create.component';
+import { TeacherListComponent } from './teachers/teacher-list/teacher-list.component';
+import { TeacherItemComponent } from './teachers/teacher-list/teacher-item/teacher-item.component';
+import { StudentCreateComponent } from './students/student-create/student-create.component';
+import { TeacherEditComponent } from './teachers/teacher-edit/teacher-edit.component';
+import { StudentEditComponent } from './students/student-edit/student-edit.component';
+import { StudentListComponent } from './students/student-list/student-list.component';
+import { StudentItemComponent } from './students/student-list/student-item/student-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TeacherCreateComponent,
+    TeacherListComponent,
+    TeacherItemComponent,
+    StudentCreateComponent,
+    TeacherEditComponent,
+    StudentEditComponent,
+    StudentListComponent,
+    StudentItemComponent
   ],
   imports: [
     BrowserModule,
