124c0d6df4bffb33e135f45256082d8d88eab3c8
Abdel-lhamid <abdelhamidbusiness@gmail.com>
2023-03-21 15:22:08 +0000

    set routing to approutingmodule. closes #6

--- Angular-Crud-Manage-teachers-students/src/app/app-routing.module.ts
+++ Angular-Crud-Manage-teachers-students/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TeacherListComponent } from './teachers/teacher-list/teacher-list.component';
+import { TeacherCreateComponent } from './teachers/teacher-create/teacher-create.component';
+import { StudentListComponent } from './students/student-list/student-list.component';
+import { StudentCreateComponent } from './students/student-create/student-create.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"teachers",component:TeacherListComponent},
+  {path:"createteacher",component:TeacherCreateComponent},
+  {path:"students",component:StudentListComponent},
+  {path:"createteacher",component:StudentCreateComponent},
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
