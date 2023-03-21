eaf5df6c10cce186985636cd1c525cc1350abc61
Abdel-lhamid <abdelhamidbusiness@gmail.com>
2023-03-21 15:29:40 +0000

    initiate teacher  student with fake data.closes #9

--- Angular-Crud-Manage-teachers-students/src/app/services/student.service.ts
+++ Angular-Crud-Manage-teachers-students/src/app/services/student.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { Student } from '../models/student.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StudentService {
+
+  students:Student[]=[
+    {
+    name:"amin",
+    email:"amin@gmail.com",
+    fillier:"ifsdm"
+    },
+    {
+      name:"walid",
+      email:"walid@gmail.com",
+      fillier:"2sdi"
+      }
+
+  ]
 
   constructor() { }
 }
--- Angular-Crud-Manage-teachers-students/src/app/services/teacher.service.ts
+++ Angular-Crud-Manage-teachers-students/src/app/services/teacher.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Teacher } from '../models/teache.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TeacherService {
+
+
+  teachers:Teacher[]=[
+    {
+    name:"youness",
+    email:"youness@gmail.com",
+    salaire:15000
+    },
+    {
+      name:"salah",
+      email:"salah@gmail.com",
+      salaire:13000
+      }
+
+  ]
 
   constructor() { }
 }
